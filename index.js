a88b1fa541eaf8db06dc605ad582a4b5019702fc
joao paulo <142436189+joaopaulodasilvavanes@users.noreply.github.com>
2023-08-29 17:21:09 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+// this is a js file
+// joao paulo
+ 
+//resposta da 1
+//sera impresso na tela o resultado f 9
+
+// 2: será impresso no console a frase da pessoa em maiusculo,ira trocar o "A" pr "I" e ira contar quantos caracteres vai  ter na sua frase
+// B exercícios de escrita do código
+
+//let email = prompt("digite seu email");
+//let nome = prompt("digite seu nome");
+//console.log("  e-mail + email +  foi registrado com sucesso  + " "seja bem Vindo");
+
+//const comida = ["pizza" , "carne" , "hamburguer" , "fritas" , "lasanha"];
+//console.log(comida);
+//console.log
+
+
+
+
